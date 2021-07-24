58aaf26d1d28dc27edfacc3c1506e07745e1f37e
jigulee <jigulee@naver.com>
2021-07-24 11:34:56 +0900

    gnb수정

---
+++ js/gnb.js
@@ -0,0 +1,25 @@
+//gnb
+var $header = $("#sub_header");
+var $gnb = $("#gnb");
+var $gnb_li = $gnb.find("li");
+var $gnb_ul = $gnb_li.find("ul");
+var $sub = $gnb_li.find(".sub");
+var speed = 500;
+
+$gnb_li.on("mouseenter focusin", function () {
+    openSub(this);
+});
+
+$gnb_li.on("mouseleave focusout", function () {
+    closeSub(this);
+});
+
+function openSub(el) {
+    $(el).children(".sub").stop().slideDown(speed);
+    $(el).children("a").addClass("on");
+}
+
+function closeSub(el) {
+    $(el).children(".sub").stop().slideUp(speed / 2);
+    $(el).children("a").removeClass("on");
+}
